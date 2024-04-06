7054f3bc1b2699f39125ba0be806f9aff9fea26e
bobandbrony <bobandbrony@outlook.com>
2024-04-06 18:59:01 +0800

    1.0

---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 询问用户想要创建的titanedge项目数量
+read -p "请输入你想要创建的titanedge项目数量（n）: " n
+
+# 询问用户输入hash值
+read -p "请输入hash值: " user_hash
+
+# 检查并拉取最新的nezha123/titan-edge镜像
+echo "正在检查并拉取最新的nezha123/titan-edge镜像..."
+docker pull nezha123/titan-edge
+
+# 查找已存在的最大项目编号
+max_existing_id=0
+for dir in ~/.titanedge*; do
+  if [[ -d $dir && $dir =~ ~/.titanedge([0-9]+)$ ]]; then
+    id=${BASH_REMATCH[1]}
+    if (( id > max_existing_id )); then
+      max_existing_id=$id
+    fi
+  fi
+done
+
+# 根据已存在的项目编号开始创建新项目
+for ((i=1; i<=n; i++))
+do
+    new_id=$((max_existing_id + i))
+    project_path="$HOME/.titanedge$new_id"
+    echo "正在处理titanedge$new_id..."
+    echo "创建目录 $project_path..."
+    mkdir -p "$project_path"
+    container_name="titanedge_$new_id"
+    echo "启动容器 $container_name..."
+    CONTAINER_ID=$(docker run -d --name $container_name -v "$project_path:/root/.titanedge" nezha123/titan-edge)
+    # 确保容器完全启动
+    sleep 10
+    echo "正在尝试绑定身份码..."
+    docker exec -it $CONTAINER_ID /bin/bash -c "titan-edge bind --hash=$user_hash https://api-test1.container1.titannet.io/api/v2/device/binding"
+done
+
+echo "所有指定的titanedge项目已成功处理。"
